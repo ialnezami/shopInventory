13d05c2e00355655adf03019ba1472b47d9e466a
ialnezami <ibrahim.alnezami@gmail.com>
2025-08-26 18:34:44 +0300

    Import FormsModule in products-list component to enable reactive forms functionality.

--- frontend/src/app/features/products/products-list/products-list.component.ts
+++ frontend/src/app/features/products/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products-list',
